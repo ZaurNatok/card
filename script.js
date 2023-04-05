a7917ba9d94a51c01c637d0f9b8ab3983f334188
ZaurNatok <61757081+ZaurNatok@users.noreply.github.com>
2023-04-05 12:15:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -229,7 +229,7 @@ function addNewClient(client) {
     .then((response) => response.json())
     .then(json => {
         
-        window.location.href='thanks.html';
+        
         console.log(json.client);
     })
     .catch((e) => {
